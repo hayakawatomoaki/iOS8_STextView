45cab130ef8ccfb45aba4438402c54e4091af23c
hayakawa <hayakawa@nttr.co.jp>
2014-09-25 20:57:40 +0900

    commit.

--- STextView/ViewController.m
+++ STextView/ViewController.m
@@ -16,7 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *note = [UILabel new];
+    note.frame = CGRectMake(10, 20, self.view.bounds.size.width-20, 150);
+    note.numberOfLines = 6;
+    note.text = @"1. scroll to last line.\n 2.tap last line then push Enter.\n 3.Oops. scrolled to top automatically.\n\n * try this code.\n[textView.layoutManager.allowsNonContiguousLayout=NO;]";
+    [self.view addSubview:note];
+    
+    
+    
+    UITextView *textView = [UITextView new];
+    textView.delegate = self;
+    textView.frame = CGRectMake(0, self.view.bounds.size.height/3, self.view.bounds.size.width, 100);
+    textView.backgroundColor = [UIColor lightGrayColor];
+    textView.font = [UIFont systemFontOfSize:20];
+    textView.text = @"ABC\nD\nD\nD\nD\nD\nD\nD\nEFG";
+
+    // try this code.
+    // textView.layoutManager.allowsNonContiguousLayout = NO;
+
+    [self.view addSubview:textView];
 }
 
 - (void)didReceiveMemoryWarning {
